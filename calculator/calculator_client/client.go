d8d5b4fbcbd816c5ab7ca232360b19d5b6b76cf2
Will Shen <will.shen@anz.com>
2019-11-07 23:04:58 +1100

    added client code for testing

---
+++ calculator/calculator_client/client.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/willshen8/gRPC-go/calculator/calculatorpb"
+	"google.golang.org/grpc"
+)
+
+func main() {
+	fmt.Println("Hello I'm a client")
+	cc, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
+
+	if err != nil {
+		log.Fatalf("Could not connect: %v", err)
+	}
+
+	defer cc.Close()
+	c := calculatorpb.NewCalculatorServiceClient(cc)
+
+	doUnary(c)
+
+}
+
+func doUnary(c calculatorpb.CalculatorServiceClient) {
+	fmt.Println("Starting to do Unary RPC...")
+	req := &calculatorpb.SumRequest{
+		A: &calculatorpb.Integer{
+			A: 100,
+		},
+		B: &calculatorpb.Integer{
+			A: 200,
+		},
+	}
+	res, err := c.Calculator(context.Background(), req)
+	if err != nil {
+		log.Fatalf("error while calling Greet RPC: %v", err)
+	}
+	log.Printf("Response from Greet: %v", res.Result)
+}
